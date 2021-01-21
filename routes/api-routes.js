1b4d9c20bed569470ca2d79c155d635cffae07f5
Daniel <dhfoss@gmail.com>
2021-01-21 12:31:30 -0500

    Added route for adding an exercise to a workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,9 +15,17 @@ router.get('/api/workouts', (req, res) => {
   db.Workout.find({})
   // db.Workout.find().sort({day: -1}).limit(1)
   .then(data => {
-      console.log(data);
       res.json(data);
-  })
-})
+  });
+});
+
+// This route updates one workout by adding an exercise
+router.put('/api/workouts/:id', (req, res) => {
+  const thisId = req.params.id;
+  db.Workout.findByIdAndUpdate(thisId, {$push: {exercises: req.body}})
+  .then(data => {
+    res.json(data);
+  });
+});
 
 module.exports = router;
